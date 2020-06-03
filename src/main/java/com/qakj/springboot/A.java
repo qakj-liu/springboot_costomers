699b9fde20a62693e35e894166a2a718d07b8f8b
tom <tom@aaa.com>
2020-06-03 16:42:27 +0800

    sfa

--- src/main/java/com/qakj/springboot/A.java
+++ src/main/java/com/qakj/springboot/A.java
@@ -1,15 +1,12 @@
 package com.qakj.springboot;
 
 public class A {
-public static void main(String[] args) {
-	System.out.println("aa");
-	
-}
-public void test1(){
-	
-}
-
-	public void test2(){
-	
+	public static void main(String[] args) {
+		System.out.println("aa");
+		
 	}
+	public void test2(){
+		
+	}
+
 }
